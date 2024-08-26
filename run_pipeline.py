73a286537251091430f94edc823e90394adc6783
Steffen Albrecht <steffen.albrecht@fli.de>
2024-08-26 11:57:34 +0200

    adding some more options to steering script

--- run_pipeline.py
+++ run_pipeline.py
@@ -7,17 +7,21 @@ import subprocess
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--version", choices=[3,1],type=int, help="version of AMR++ pipeline (1: replication of old galaxy v1 workflow, 3: default \"new\" version.)")
+    parser.add_argument("--version", choices=[3,1], default=1, type=int, help="version of AMR++ pipeline (1: replication of old galaxy v1 workflow, 3: default \"new\" version.)")
     parser.add_argument("--reads", type=str, help="path to read pairs or alternatively a file containing a list of pairs.")
     parser.add_argument("--profile",choices=["conda","local"], default="local")
     parser.add_argument("--dedup", action="store_true", help="specfify in order to enable deduplication in AMR++ pipeline.")
     parser.add_argument("--debug", action="store_true", help="do not execute anything. just print the commands for debugging.")
     parser.add_argument("--output", type=str, help="optional: specify where to store results. default is wherever reads are located")
     parser.add_argument("--threads", type=int, default=4, help="number of threads parameter passed on to nextflow")
-    parser.add_argument("--host", help="path to host genome")
+    parser.add_argument("--host", default="data/host/chr21.fasta.gz", help="path to host genome fasta for host-removal.")
+    parser.add_argument("--snp", action="store_true", help="Perform SNP analysis.")
+    parser.add_argument("--pipeline", default="standard_AMR", choices=["standard_AMR","standard_AMR_wKraken"], help="specify which AMR++ pipeline you want to run.")
     args = parser.parse_args()
 
     check_tools = ["nextflow"]
+
+    kraken_db = os.environ.get("KRAKEN2_DB", "/home/admin/seq_tools/kraken2/kraken2_DB")
 
     if args.profile == "local":
         check_tools += ["bwa", "samtools"]
@@ -30,12 +34,14 @@ if __name__ == "__main__":
 
     cmd = "nextflow run main_AMR++.nf -profile {}".format(args.profile)
 
-    cmd += " --pipeline standard_AMR "
+    cmd += " --pipeline standard_AMR_wKraken"
 
     if args.version == 1:
         cmd += " --legacy Y --slidingwindow '4:20' --threshold 1 --min 1 --skip 1 "
     if args.dedup:
         cmd += " --deduped N "
+
+    cmd += " --kraken_db \"{}\"".format(kraken_db)
 
     cmd += " --threads {} ".format(args.threads)
     
