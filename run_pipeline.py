7765ab029ee1ee541251963ed8a332617cde2d19
Steffen Albrecht <steffen.albrecht@fli.de>
2024-09-03 12:59:43 +0200

    params are passed as json now to be able to make nextflow happy when using null

--- run_pipeline.py
+++ run_pipeline.py
@@ -4,7 +4,7 @@ import argparse
 import os
 import subprocess
 from pathlib import Path
-
+import json
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     parser.add_argument("--debug", action="store_true", help="do not execute anything. just print the commands for debugging.")
     parser.add_argument("--output", type=str, help="optional: specify where to store results. default is wherever reads are located")
     parser.add_argument("--threads", type=int, default=10, help="number of threads parameter passed on to nextflow")
-    parser.add_argument("--host", default="data/host/chr21.fasta.gz", help="path to host genome fasta for host-removal.")
+    parser.add_argument("--host", default=None, help="path to host genome fasta for host-removal.")
     parser.add_argument("--snp", action="store_true", help="Perform SNP analysis.")
     parser.add_argument("--pipeline", default="standard_AMR", choices=["standard_AMR","standard_AMR_wKraken"], help="specify which AMR++ pipeline you want to run.")
     args = parser.parse_args()
@@ -26,10 +26,14 @@ if __name__ == "__main__":
     if(not os.path.exists(user_workdir)):
         print("creating user workdir: {}".format(user_workdir))
         os.makedirs(user_workdir)
- 
+    
     def resolve_path(rel):
         return Path(rel).expanduser().resolve()
     __script_dir__ = Path(__file__).resolve().parent
+
+    # add additional or overwrite params in params.json
+    params_file_path = resolve_path(user_workdir.joinpath("params.json"))
+    params = {}
 
     kraken_db = os.environ.get("KRAKEN2_DB", "/home/admin/seq_tools/kraken2/kraken2_DB")
     kraken_db = resolve_path(kraken_db)
@@ -43,25 +47,44 @@ if __name__ == "__main__":
         except BaseException:
             raise RuntimeError("Could not find depency \"{}\". Make sure your environment is setup correctly.".format(tool))
 
-    cmd = "nextflow run {}/main_AMR++.nf -profile {}".format(__script_dir__, args.profile)
+    cmd = "nextflow run {}/main_AMR++.nf -params-file {} -profile {}".format(__script_dir__, params_file_path, args.profile)
 
-    cmd += " --pipeline {} ".format(args.pipeline)
+    #cmd += " --pipeline {} ".format(args.pipeline)
+    params["pipeline"] = args.pipeline
+
+    if args.host is not None:
+        params["host"] = resolve_path(args.host).as_posix()
+        params["host-index"] = "REPLACENULL"
 
     if args.version == 1:
-        cmd += " --legacy Y --slidingwindow '4:20' --threshold 1 --min 1 --skip 1 "
+        params["legacy"]="Y"
+        params["slidingwindow"] = "4:20"
+        params["threshold"] = "1"
+        params["min"] = "1"
+        params["skip"] = "1"
+        #cmd += " --legacy Y --slidingwindow '4:20' --threshold 1 --min 1 --skip 1 "
 
     if args.dedup:
-        cmd += " --deduped Y "
+        params["deduped"] = "Y"
+        #cmd += " --deduped Y "
 
     if args.snp:
-        cmd += " --snp Y "
+        params["snp"] = "Y"
+        #cmd += " --snp Y "
 
-    cmd += " --kraken_db \"{}\"".format(kraken_db)
+    #cmd += " --kraken_db \"{}\"".format(kraken_db)
+    params["kraken_db"] = kraken_db.as_posix()
 
-    cmd += " --kraken_memory_mapping \"Y\" "
+    #cmd += " --kraken_memory_mapping \"Y\" "
+    params["kraken_memory_mapping"] = "Y"
 
-    cmd += " --threads {} ".format(args.threads)
-    
+    #cmd += " --threads {} ".format(args.threads)
+    params["threads"] = args.threads
+
+    json.dump(params,open(params_file_path,"w"))
+
+    os.system("sed -i 's/\"REPLACENULL\"/null/g' {}".format(params_file_path))
+
     def process_reads(cmd_, reads_pattern, output_dir):
         reads_pattern = resolve_path(reads_pattern)
         cmd_ += " --reads \"{}\" ".format(reads_pattern)
